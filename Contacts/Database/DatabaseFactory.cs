597406768e2d32e21d6636aa7c63c0eab9d0cb1b
Mini-Thomas <40882707+Mini-Thomas@users.noreply.github.com>
2018-07-21 21:30:49 +0530

    Add files via upload

---
+++ Contacts/Database/DatabaseFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using S
+
+namespace Database
+{
+    static class DatabaseFactory
+    {
+        public static IDbConnection CreateDbConnection(string server, string db, string user, string pass)
+        {
+            DbConnectionStringBuilder builder = new SqlConnectionStringBuilder
+            {
+                DataSource = server,
+                InitialCatalog = db,
+                UserID = user,
+                Password = pass
+            };
+
+            return new SqlConnection(builder.ConnectionString);
+        }
+
+        public static IDbCommand CreateCommand(string storedProcName, params DbParameter[] parameters)
+        {
+            var storedProcedure = new SqlCommand(storedProcName)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+
+            storedProcedure.Parameters.AddRange(parameters);
+            return storedProcedure;
+        }
+    }
+}
---
+++ Contacts/Database/IDatabaseConnectionManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database
+{
+    interface IDatabaseConnectionManager
+    {
+        void Open();
+
+        IDbConnection Connection { get; }
+
+        void Close();
+    }
+}
